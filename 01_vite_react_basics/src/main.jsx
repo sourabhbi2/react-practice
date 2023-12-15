a2fc20bf99da1c99e1a0de731e37eb3bac87d438
sourabhbi2 <sourabhkumar1109@gmail.com>
2023-12-15 14:40:18 +0530

    custom react in vite+reactJS

--- 01_vite_react_basics/src/main.jsx
+++ 01_vite_react_basics/src/main.jsx
@@ -1,12 +1,21 @@
-
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Demo from './Demo.jsx'
 
+const reactElement = React.createElement(
+'a',
+{href: 'https://google.com', target: '_blank'},
+'click me to visit google'
+)
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
-    <App />
-    <Demo />
-    </>  
+  
+  reactElement
+    /* <App />
+    <Demo /> */
+     
 )
