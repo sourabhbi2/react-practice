50c8668932e8af5ab85fca58df42b9508cb59804
sourabhbi2 <sourabhkumar1109@gmail.com>
2023-12-16 19:09:48 +0530

    Tailwind init

---
+++ 03_tailwindProps/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1 className='bg-blue-400 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1>
+
+     <Card username= "Sourabh" />
+     <Card username= 'Sairah'/>
+     
+    </>
+  )
+}
+
+export default App
